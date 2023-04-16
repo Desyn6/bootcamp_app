3dbda4ccf1690162869000f8f01555ef36056b7a
Denny <Dennyng1989@hotmail.com>
2023-04-16 15:17:08 -0700

    Parameterize queries

--- students.js
+++ students.js
@@ -1,6 +1,15 @@
-const args = process.argv.slice(2);
-const cohortQuery = args[0];
-const queryLimit = args[1];
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
 const { Pool } = require('pg');
 
@@ -11,15 +20,9 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort_id
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortQuery}%'
-LIMIT ${queryLimit || 5};
-`)
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 });
--- teachers.js
+++ teachers.js
@@ -1,3 +1,15 @@
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+const query = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1;
+`;
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,14 +19,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name
-  FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}';
-`)
+pool.query(query, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name}: ${user.teacher}`)
